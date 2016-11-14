35a66d68f4e1aab02d3fef83633e041eb8663df6
Kshitiz <kshitiz.dange@gmail.com>
2016-11-13 23:42:43 -0500

    fix typos

--- data/preprocess_data.sh
+++ data/preprocess_data.sh
@@ -1,4 +1,4 @@
 # preprocess raw data
-python preprocess_stsa.py ./raw/ ../../data/word2vec/GoogleNews-vectors-negative300.bin
+python preprocess_stsa.py ./raw/ ./w2v/GoogleNews-vectors-negative300.bin
 # extract rule features
 python logicnn_features.py ./stsa.binary.p 
