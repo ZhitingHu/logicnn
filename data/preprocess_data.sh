421b3d86799c0ea294298575e04c2d18c1e3a733
Kshitiz <kshitiz.dange@gmail.com>
2016-11-13 23:40:05 -0500

    deleted incomplete codes

--- data/preprocess_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'preprocessing sst2 raw data ...'
-python stsa_preprocessor.py sst/ word2vec/GoogleNews-vectors-negative300.bin
-
-echo 'extracting features ...'
-python feature_extractor.py ./stsa.binary.p 
-
-echo 'data preprocessing done.'
--- run.sh
+++
@@ -1,4 +0,0 @@
-for i in 1 2 3 4 5
-do
-    THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python logicnn_sentiment_bak.py -nonstatic -word2vec
-done
